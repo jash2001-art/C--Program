6c8c409bf24eae11a28e11efb88acd0e02a2db96
jash2001-art <63535402+jash2001-art@users.noreply.github.com>
2020-05-20 15:10:00 +0530

    Created by Jash Patel
    Created on C plus plus

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+double firstnumber, secondnumber, Product of two number;
+cout << "Enter two numbers:";
+cin >> firstnumber >> secondnumber;
+Product of two number= firstnumber * secondnumber;
+cout << "Product=" << Product of two number;
+return 0;
+}
